eb38e8471b1fa3670751da94100a186c4712cfb3
Mmesoma <mmesomaonwusika@gmail.com>
2025-06-14 13:13:51 +0100

    Update

--- bts.js
+++ bts.js
@@ -7,5 +7,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
+window.addEventListener('DOMContentLoaded', () => {
+  const markers = document.querySelectorAll('.map-marker');
+  markers.forEach((marker, index) => {
+    setTimeout(() => {
+      marker.style.opacity = 1;
+    }, index * 200);
+  });
+});
+
 
 
